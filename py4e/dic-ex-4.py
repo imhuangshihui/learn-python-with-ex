b2bf41113cbcf4874c41fc381c4c18ac1f22d75d
imhuangshihui <76728625+imhuangshihui@users.noreply.github.com>
2021-10-18 14:06:34 +0800

    Update dic-ex-4.py

--- py4e/dic-ex-4.py
+++ py4e/dic-ex-4.py
@@ -1,3 +1,14 @@
+'''
+Exercise 4: Add code to the above program to figure out who has the
+most messages in the file. After all the data has been read and the dictionary has been created, look through the dictionary using a maximum
+loop (see Chapter 5: Maximum and minimum loops) to find who has
+the most messages and print how many messages the person has.
+Enter a file name: mbox-short.txt
+cwen@iupui.edu 5
+Enter a file name: mbox.txt
+zqian@umich.edu 195
+'''
+
 filename = input("Enter file name: ")
 try:
     fhand = open(filename)
